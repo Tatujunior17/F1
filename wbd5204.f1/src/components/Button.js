6f1f3c0ddf9a149f32fb83f93848b30ff493b223
Tatujunior17 <ivane_rodrigues@hotmail.com>
2021-04-23 10:54:57 +0200

    Devoir F1

---
+++ wbd5204.f1/src/components/Button.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledBtn = styled.button`
+padding: 6px 12px;
+background: ${props => props.disabled ? '#E8E8E8' : '#FA541C'};
+  border: none;
+  border-radius: 4px;;
+  color: ${props => props.disabled ? '#BFBFBF' : '#FAFAFA'};
+  transition: all 0.3s ease-in-out;
+  cursor: ${props => !props.disabled && 'pointer'};
+  
+  &:hover {
+  background: ${props => !props.disabled && '#ff7a45'};
+  }
+	`;
+
+
+// This theme
+const Button = ({disabled = false, onClick, type, htmlType, style, children}) => (
+    <StyledBtn
+        disabled={disabled}
+        type={type}
+        style={{...style}}
+        htmlType={htmlType}
+        onClick={onClick}
+    >
+        {children}
+    </StyledBtn>
+);
+
+export default Button;
---
+++ wbd5204.f1/src/components/Modal.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled from "styled-components";
+
+const StyledModal = styled.div`
+	cursor: pointer;
+	display:  flex;
+	align-items: center;
+	justify-content: centerM
+	width: 30%; 
+	height: 30%; 
+
+	background-color: rgba(0,0,0,0.4);
+	`;
+
+const Modal = ({visible,onCancel,onOk,title, children}) => {
+
+    return visible ? (
+        <>
+            <StyledModal>
+                <div id="MyModal">
+                    {title}
+                    <div id="contentModal">
+                        <p>{children}</p>
+                        <button id="okModal" onClick={onOk}>Ok</button>
+                        <button id="closeModal" onClick={onCancel}>Annuler</button>
+                    </div>
+                </div>
+            </StyledModal>
+        </>
+    ) : null
+};
+
+
+export default Modal;
+
